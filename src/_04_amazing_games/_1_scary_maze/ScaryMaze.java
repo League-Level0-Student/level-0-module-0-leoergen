7de1c2bd195cdb4e1ae722db04897486cba0eee0
root <root@204322c23630>
2020-07-15 01:28:34 +0000

    not so scary maze

--- src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
+++ src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
@@ -66,7 +66,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		
 		
 		//7. Make a new int variable for the background color of the maze
-int backgroundColor=-10140895;
+int backgroundColor=-65536;
 		
 		//8. Run the program and move the mouse over the BACKGROUND COLOR. 
 		//   Use the number that is printed to the console to set the background color variable 
@@ -78,10 +78,15 @@ int backgroundColor=-10140895;
 		
 			//9. If the mouse falls off the path (which means it is on the background color)
 			//		call the scare method - scare();
-			
+			if(mouseColor==backgroundColor) {scare();
+				
+			}
 			//13.  If the mouse is on the end color, pop up a message to tell them they won!
 			//    (you will need to get the number of the END COLOR by moving the mouse over it)
-
+if (mouseColor== -4354560) {JOptionPane.showMessageDialog(null," You Win!!!");
+	
+	
+}
 					
 			
 		}	
@@ -108,7 +113,7 @@ int backgroundColor=-10140895;
 
 		//12. Find a scary image and drop it into the section5 package. 
 		//    Use the showScaryImage method below and send it the name of your picture file
-		
+		showScaryImage("unnamed.jpg");
 		
 	}
 	
--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -14,19 +14,19 @@ public class DragonFight {
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 
 		// 1. Create some variables to hold health levels
-		
+		int playerHealth=100;
 			// playerHealth to store your health - set it equal to 100
 	
 			// dragonHealth to store the dragon's health - set it equal to 100
-
+int dragonHealth=100;
 		// 2. Create some variables to hold the attack strengths. These will be given different values later. 
-		
+		int playerAttack=0;
 			// playerAttack to store the damage the player's attack will do - set it equal
 			// to 0 for now.
 	
 			// dragonAttack to store the damage the dragon's attack will do - set it equal
 			// to 0 for now.
-
+int dragonAttack=0;
 		
 		//  This while statement will cause the game to repeat until the player or dragon run out of health
 		while (true) {
@@ -35,31 +35,35 @@ public class DragonFight {
 
 				// 3. Ask the player in a pop-up if they want to attack the dragon with a yell
 				// or a kick
-	
+			String input=JOptionPane.showInputDialog("do you want to yell or kick?");
 				// 4. If they typed in "yell":
-	
+	if(input.equals("yell")) {
 					// -- Find a random number between 0 and 10 and store it in playerAttack. Use
 					// ran.nextInt(10)
-		
+	playerAttack=ran.nextInt(10);
 					// -- Subtract the player attack value from the dragon's health
-	
+	dragonHealth-=playerAttack;
+	}
 				// 5. If they typed in "kick":
-	
+	if(input.equals("kick")) {
 					// -- Find a random number between 0 and 25 and store it in playerAttack.
-		
+		playerAttack=ran.nextInt(25);
 					// -- Subtract the player attack value from the dragon's health
-
+		dragonHealth-=playerAttack;
+	}
 			// THE DRAGON RETALIATES
 
 				// 6. Find a random number between 0 and 35 and store it in dragonAttack
-	
+	dragonAttack=ran.nextInt(35);
 				// 7. Subtract the dragon attack value from the player's health
-
+playerHealth-=dragonAttack;
 			// ASSESS THE DAMAGE CAUSED 
 
 				// 8. If the player's health is less than or equal to 0
 				// call the playerLost() method
+	if (playerHealth<=0) {
 	
+	}
 			
 				// 9. Else if the dragon's health is less than or equal to 0
 				// call the dragonLost() method
